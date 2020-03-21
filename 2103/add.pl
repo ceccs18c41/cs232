f9c5b2ab7817c17e24ae86b45523e7b25e590f7d
NAJEEB <60335647+ceccs18c41@users.noreply.github.com>
2020-03-21 22:24:49 +0530

    Add files via upload

---
+++ 2103/add.pl
@@ -0,0 +1,7 @@
+print "\nEnter two numbers: ";
+# read elements
+$a = <>; $b = <>;
+# compute sum
+$c = $a +$b;
+# print sum
+print "\nSum is $c\n";
---
+++ 2103/arrays.pl
@@ -0,0 +1,9 @@
+
+# arrays
+@langs = ('c++', 'c', 'java', 'python', 'javascript');
+
+# $lang iterates through each element in @lang
+foreach $lang (@langs) 
+{
+	print "\n$lang";
+}
